7837c2316fac968757101147d18876f34275c4d9
vtcstudent2021 <210086768@stu.vtc.edu.hk>
2024-04-17 22:03:08 +0800

    123

---
+++ core/urls.py
@@ -0,0 +1,15 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import include, path
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", include("store.urls", namespace="store")),
+    path("basket/", include("basket.urls", namespace="basket")),
+    path('blog/', include('blog.urls')),
+    path('account/', include('users.urls')),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
